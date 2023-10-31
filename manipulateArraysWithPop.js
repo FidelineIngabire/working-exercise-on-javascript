22a0aa1b984a91b677e78d013db3fb779d5300d0
Fideline Ingabire <fidelining09@gmail.com>
2023-11-01 00:04:48 +0200

    remmoving from array

---
+++ manipulateArraysWithPop.js
@@ -0,0 +1,7 @@
+let myArray = [1,2,3]
+let myRemovedFrommyArray = myArray.pop(2)
+console.log(myArray)
+
+let myarray = [["amoris",3], ["school",2]]
+let myremovedFrommyarray = myarray.pop(1)
+console.log(myarray)
